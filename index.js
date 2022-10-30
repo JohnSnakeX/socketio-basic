303590e60b01a91056eb0b612877952b90a63c09
John Prada <jhonprada@outlook.com>
2022-10-30 11:53:20 -0500

    Added html and connections

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-//Servidor de express
-const app = require('express')();
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-//Servidor de sockets
-const server = require('http').createServer(app);
+//Mimic old __dirname and __filename
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-//configuracion de socket.io
-const io = require('socket.io')(server);
-io.on('connection', () => { /* … */ });
-server.listen(8080 , () => {
+
+
+const app = express();
+//Servidor de socket
+const httpServer = createServer(app);
+
+//Configuración del socket
+const io = new Server(httpServer, {
+  // ...
+});
+//Desplegar el directorio público
+
+app.use(express.static(__dirname + "/public"));
+io.on("connection", (socket) => {
+    socket.emit("mensaje-bienvenida", {
+        msg: "Bienvenido al servidor",
+        fecha: new Date(),
+    });
+    socket.on('mensaje-cliente',payload=>{
+        console.log(payload);
+    })
+});
+
+httpServer.listen(8080 , () => {
     console.log('Servidor corriendo en http://localhost:8080');
 });
+
+
