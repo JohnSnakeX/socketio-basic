282676e8aa4fdb5ed8f5136f4b7372c775feea1e
John Prada <jhonprada@outlook.com>
2022-11-02 17:20:26 -0500

    chat models

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import ServerIO from "./models/server.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-//Mimic old __dirname and __filename
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-
-const app = express();
-//Servidor de socket
-const httpServer = createServer(app);
-
-//Configuración del socket
-const io = new Server(httpServer, {
-  // ...
-});
-//Desplegar el directorio público
-
-app.use(express.static(__dirname + "/public"));
-io.on("connection", (socket) => {
-    socket.emit("mensaje-bienvenida", {
-        msg: "Bienvenido al servidor",
-        fecha: new Date(),
-    });
-    socket.on('mensaje-cliente',payload=>{
-        console.log(payload);
-    })
-});
-
-httpServer.listen(8080 , () => {
-    console.log('Servidor corriendo en http://localhost:8080');
-});
-
-
+const server = new ServerIO();
+server.execute();
---
+++ models/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+import Sockets from "./sockets.js";
+
+class ServerIO {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    //Http server
+    this.server = createServer(this.app);
+    //Configuraciones de socket
+    this.io = new Server(this.server, {
+      /* configuraciones */
+    });
+  }
+
+  middlewares() {
+    //Mimic old __dirname and __filename with ES6
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    //Desplegar el directorio público
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
+
+  //Configurar Sockets
+  configurarSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    //Inicializar middlewares
+    this.middlewares();
+    //init server
+    this.server.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+    this.configurarSockets();
+  }
+}
+
+export default ServerIO;
